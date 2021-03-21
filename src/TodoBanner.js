710f20e376f957112f57be987768e7d6a778f875
vishaltyagi126 <vishaltyagi126@gmail.com>
2021-03-21 18:47:23 +0000

    deploy my app

--- src/TodoBanner.js
+++ src/TodoBanner.js
@@ -4,7 +4,7 @@ export  class TodoBanner extends Component
 {
     render =() => 
 
-    <h4 className="bg-primary text-center text-white p-2">
+    <h4 className="bg-primary text-center text-white p-3">
             { this.props.name }, you have 
             ({ this.props.tasks.filter(t=> !t.done ).length }) tasks to complete
     </h4>
